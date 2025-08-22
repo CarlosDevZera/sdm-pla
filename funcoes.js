be6d340f7e350cf3ec610233285495b3667ee8a8
Carlos Alberto Cardoso de Oliveira <825111293@ulife.com.br>
2025-08-22 11:41:45 -0300

    Subindo o arquivo funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,27 @@
+
+//funcao declarativa
+
+function saudacao(nome) {
+    return `Ola, ${nome}!`
+}
+
+console.log(saudacao("Carlos"))
+
+//funcao anonima
+
+const soma = function(a, b) {
+    return a + b
+}
+
+console.log(soma(5,10))
+
+
+function soma2 (a, b) {
+    return a + b
+}
+
+console.log(soma2(5,15))
+
+
+
+
