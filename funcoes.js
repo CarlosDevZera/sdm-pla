3641590f7c1b02ae9c47f6eb5f0cdb4bb6a9d50c
Carlos Alberto Cardoso de Oliveira <825111293@ulife.com.br>
2025-08-29 11:16:27 -0300

    Alterei o arquivo funções

--- funcoes.js
+++ funcoes.js
@@ -1,27 +1,48 @@
 
 //funcao declarativa
 
-function saudacao(nome) {
-    return `Ola, ${nome}!`
-}
+//function saudacao(nome) {
+//    return `Ola, ${nome}!`
+//}
 
-console.log(saudacao("Carlos"))
+//console.log(saudacao("Carlos"))
 
 //funcao anonima
 
-const soma = function(a, b) {
-    return a + b
+//const soma = function(a, b) {
+//    return a + b
+//}
+
+//console.log(soma(5,10))
+
+
+//function soma2 (a, b) {
+//    return a + b
+//}
+
+//console.log(soma2(5,15))
+
+
+//const multiplicar = (a, b) => a * b
+
+//console.log(multiplicar(5, 5))
+
+
+function ExibirIdade(idade) {
+    return `Sua idade é ${idade} anos!`
 }
 
-console.log(soma(5,10))
 
-
-function soma2 (a, b) {
-    return a + b
+const ExibirAltura = function(altura) {
+    return altura
 }
 
-console.log(soma2(5,15))
+
+const ExibirCurso = (curso) => curso
 
 
+const ExibirTudo = (idade, altura, curso) => {
+    console.log(ExibirIdade(idade), ExibirAltura(altura), ExibirCurso(curso));
+}
 
-
+ExibirTudo(31, 1.88, "Sistema de Informação");
