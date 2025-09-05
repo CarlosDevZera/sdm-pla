e79595aed4670cac6f343885c7d406f832d8034d
Carlos Alberto Cardoso de Oliveira <825111293@ulife.com.br>
2025-09-05 10:46:40 -0300

    Arquivo da aula do dia 05/09

---
+++ colecoes.js
@@ -0,0 +1,15 @@
+
+let numeros = [1, 2, 3, 4, 5]
+//console.log(numeros)
+
+let frutas = ["maça", "banana", "laranja"]
+frutas.unshift("morango")
+frutas.push("uva")
+console.log(frutas)
+
+frutas.forEach((frutas, index) => {
+    console.log(`Indice: ${index}  ${frutas}`)
+})
+
+
+
